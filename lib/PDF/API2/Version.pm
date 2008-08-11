2b8cb77877ebc1fc0192ba45a75b2978bf416a53
Alfred Reibenschuh <alfredreibenschuh@gmx.net>
2008-08-11 01:37:10 +0000

    Import of AREIBENS/PDF-API2-0.71.001 from CPAN.
    gitpan-cpan-distribution: PDF-API2
    gitpan-cpan-version:      0.71.001
    gitpan-cpan-path:         AREIBENS/PDF-API2-0.71.001.tar.gz
    gitpan-cpan-author:       AREIBENS
    gitpan-cpan-maturity:     released

--- lib/PDF/API2/Version.pm
+++ lib/PDF/API2/Version.pm
@@ -9,14 +9,14 @@
 package PDF::API2::Version;
 BEGIN {
     use vars qw( $VERSION %CVersion );
-    $VERSION = '0.71';
+    $VERSION = '0.71.001';
     %CVersion = (
-        'vFredo' => 'PDF::API2 0.71',
-        'vHex' => '0x00047000',
-        'vLong' => '0.71',
-        'vPerl' => '0.71',
+        'vFredo' => 'PDF::API2 0.71.001',
+        'vHex' => '0x00047001',
+        'vLong' => '0.71.001',
+        'vPerl' => '0.71.001',
         'vShort' => '0.71',
-        'vWeb' => 'PDF::API2/0.71',
+        'vWeb' => 'PDF::API2/0.71.001',
     );
 }
 1;
@@ -32,12 +32,12 @@ PDF::API2::Version
 
 =head1 VERSION
 
-  vFredo: PDF::API2 0.71 
-    vHex: 0x00047000 
-   vLong: 0.71 
-   vPerl: 0.71 
+  vFredo: PDF::API2 0.71.001 
+    vHex: 0x00047001 
+   vLong: 0.71.001 
+   vPerl: 0.71.001 
   vShort: 0.71 
-    vWeb: PDF::API2/0.71 
+    vWeb: PDF::API2/0.71.001 
 
 =cut
 
