e73834aa79a9bf5be1acfaf3838336abb29d0745
Alfred Reibenschuh <alfredreibenschuh@gmx.net>
2008-11-22 12:41:38 +0000

    Import of AREIBENS/PDF-API2-0.72.002 from CPAN.
    gitpan-cpan-distribution: PDF-API2
    gitpan-cpan-version:      0.72.002
    gitpan-cpan-path:         AREIBENS/PDF-API2-0.72.002.tar.gz
    gitpan-cpan-author:       AREIBENS
    gitpan-cpan-maturity:     released

--- tarhelper.pm
+++ tarhelper.pm
@@ -1,7 +1,56 @@
-package main;
-
 use File::Find;
 use Archive::Tar;
+
+package Archive::Tar::File;
+
+sub _new_from_file {
+    my $class       = shift;
+    my $path        = shift;        
+    return unless defined $path;
+    my $type        = __PACKAGE__->_filetype($path);
+    my $data        = '';
+   READ: { 
+        unless ($type == DIR ) {
+            my $fh = IO::File->new;
+            unless( $fh->open($path) ) {
+                last READ if $type == SYMLINK;
+                return;
+            }
+            binmode $fh;
+            $data = do { local $/; <$fh> };
+            close $fh;
+        }
+    }
+    my @items       = qw[mode uid gid size mtime];
+    my %hash        = map { shift(@items), $_ } (lstat $path)[2,4,5,7,9];
+    $hash{size}     = 0 if ($type == DIR or $type == SYMLINK);
+    $hash{mtime}    -= TIME_OFFSET;
+    $hash{mode}     = 0640;
+    my $obj = {
+        %hash,
+        name        => '',
+        chksum      => CHECK_SUM,
+        type        => $type,
+        linkname    => ($type == SYMLINK and CAN_READLINK)
+                            ? readlink $path
+                            : '',
+        magic       => MAGIC,
+        version     => TAR_VERSION,
+        uname       => UNAME->( $hash{uid} ),
+        gname       => GNAME->( $hash{gid} ),
+        devmajor    => 0,   # not handled
+        devminor    => 0,   # not handled
+        prefix      => '',
+        data        => $data,
+    };
+    bless $obj, $class;
+    my($prefix,$file) = $obj->_prefix_and_file( $path );
+    $obj->prefix( $prefix );
+    $obj->name( $file );
+    return $obj;
+}
+
+package main;
 
 sub mytar
 {
